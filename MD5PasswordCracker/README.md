2dad88ff7f81b9196efb38d866287f75efa8b8c0
wyk9787 <wangyika@grinnell.edu>
2019-03-08 10:24:35 -0600

    Update Password Cracker

---
+++ MD5PasswordCracker/README.md
@@ -0,0 +1,11 @@
+# MD5 Password Cracker
+
+A parallel password cracker that uses brute force to crack MD5 passwords with
+fixed length (default is 6) using multiple threads.
+
+## Usage and Example
+
+```
+make clean all
+./password-cracker passwords.txt
+```
---
+++ MD5PasswordCracker/obj/password-cracker.d
@@ -0,0 +1,10 @@
+obj/password-cracker.o: password-cracker.c \
+  /usr/local/opt/openssl/include/openssl/md5.h \
+  /usr/local/opt/openssl/include/openssl/e_os2.h \
+  /usr/local/opt/openssl/include/openssl/opensslconf.h
+
+/usr/local/opt/openssl/include/openssl/md5.h:
+
+/usr/local/opt/openssl/include/openssl/e_os2.h:
+
+/usr/local/opt/openssl/include/openssl/opensslconf.h:
